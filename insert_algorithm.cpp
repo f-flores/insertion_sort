412870ad7eb15e6d13fc924bb846ea766d833432
Fabian Flores <ffflores1@outlook.com>
2018-01-17 17:30:49 -0500

    Modified index type from int to unsigned int.

--- insert_algorithm.cpp
+++ insert_algorithm.cpp
@@ -5,15 +5,15 @@
 
 void insertion_sort(vector<int>& v)
 {
-	int key {0}, j {0};
+	unsigned int key {0}, j {0};
 	for (unsigned int i = 0; i < v.size();i++)
 	{
 		 key = v[i];
 		 j = i - 1;
 		 while ((j >= 0) && (v[j] > key))
 		 {
-				 v[j + 1] = v[j];
-				 j = j - 1;
+			 v[j + 1] = v[j];
+			 j = j - 1;
 		 }
 		 v[j + 1] = key;
 	}
